dcb6cb86e7bc9f24b5a2282c16eba6c87f7d9b51
nataliagarcia <nataliagarkcia@hotmail.com>
2016-09-24 12:46:05 -0400

    changes3

--- app/routing/html-route.js
+++ app/routing/html-route.js
@@ -18,6 +18,6 @@ module.exports = function(app){
 	//	res.sendFile(path.join(__dirname + '/../public/home.html'));
 	//});
 	app.get('/', function(req, res){
-		res.sendFile(path.join(__dirname + '/../public/home.html'));
+		res.sendFile(path.join(__dirname + '/../public/survey.html'));
 	});
 }
